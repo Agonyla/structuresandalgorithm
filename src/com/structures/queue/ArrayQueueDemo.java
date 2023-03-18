b3197cc64b4eb166c930ae1c6d1e89f90604987e
Agony <gcl1197134883@gmail.com>
2023-03-18 22:38:56 +0800

    queue

---
+++ src/com/structures/queue/ArrayQueueDemo.java
@@ -0,0 +1,52 @@
+package com.structures.queue;
+
+/**
+ * @Author Agony
+ * @Create 2023/3/17 18:08
+ * @Version 1.0
+ */
+public class ArrayQueueDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class ArrayQueue {
+
+
+    // 创建队列的构造器
+    public ArrayQueue(int arrMaxSize) {
+
+    }
+
+    // 判断队列是否满
+    public boolean isFull() {
+        return false;
+    }
+
+    // 判断队列是否为空
+    public boolean isEmpty() {
+        return true;
+    }
+
+    // 添加数据到队列
+    public void addQueue(int n) {
+
+    }
+
+    // 获取队列的数据, 出队列
+    public int getQueue() {
+        return 1;
+    }
+
+    // 显示队列的所有数据
+    public void showQueue() {
+
+    }
+
+    // 显示队列的头数据， 注意不是取出数据
+    public int headQueue() {
+        return 1;
+    }
+}
